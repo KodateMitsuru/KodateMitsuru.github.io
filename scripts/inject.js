adb52aceaf948f13e754fe262b2cb970b75e620a
KodateMitsuru <79790550+KodateMitsuru@users.noreply.github.com>
2022-09-22 17:31:34 +0800

    Wexagonal Upload at 9/22/2022, 9:31:34 AM

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,2 +1,2 @@
 hexo.extend.injector.register('body_end', '<script src="/js/timecheck.js"></script>', 'post');
-hexo.extend.injector.register('body_begin', '<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>', 'default');
+hexo.extend.injector.register('head_begin', '<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>', 'default');
