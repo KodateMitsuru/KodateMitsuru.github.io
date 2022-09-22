ee393235ce0bad4b11b85e8f6b46b7d3eebbcf5c
KodateMitsuru <hh178239456@gmail.com>
2022-09-22 17:07:41 +0800

    add js

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,2 +1,2 @@
 hexo.extend.injector.register('body_end', '<script src="/js/timecheck.js"></script>', 'post');
-hexo.extend.injector.register('body_end', '<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>', 'default');
+hexo.extend.injector.register('head_end', '<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>', 'default');
