ab76ab4d5161562763d4734e1b9e0525441f86e2
KodateMitsuru <79790550+KodateMitsuru@users.noreply.github.com>
2022-09-22 12:52:14 +0800

    Update inject.js

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,1 +1,2 @@
 hexo.extend.injector.register('body_end', '<script src="/js/timecheck.js"></script>', 'post');
+hexo.extend.injector.register('body_end', '<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>', 'default');
