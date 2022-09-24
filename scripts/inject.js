3022219e1464ad9971175cb0d4ca1203891cec53
KodateMitsuru <hh178239456@gmail.com>
2022-09-24 20:40:50 +0800

    fix

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,4 +1,4 @@
 hexo.extend.injector.register('body_end', '<script src="/js/timecheck.js"></script>', 'post');
 hexo.extend.filter.register('theme_inject', function(injects) {
-  injects.head.raw('default', '<script src="https://gcore.jsdelivr.net/gh/kodatemitsuru/live2d-widget@v0.0.1/autoload.js"></script>');
+  injects.head.raw('default', '<script src="https://gcore.jsdelivr.net/gh/kodatemitsuru/live2d-widget@v0.0.2/autoload.js"></script>');
 });
