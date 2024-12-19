eef04558b3eecfd94180406a3a5a416667babe08
kodatemitsuru <hh178239456@gmail.com>
2024-12-19 16:01:19 +0800

    添加 Giscus 评论组件，更新站点配置，新增博客文章，移除草稿文件，配置图像文件使用 Git LFS

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import type {
+  CommentConfig,
   LicenseConfig,
   NavBarConfig,
   ProfileConfig,
@@ -7,16 +8,16 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  title: 'KodateMitsuru',
+  subtitle: 'ad astra per aspera',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -29,11 +30,11 @@ export const siteConfig: SiteConfig = {
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: '/favicon/kodatemitsuru.ico',    // Path of the favicon, relative to the /public directory
+      // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
@@ -44,34 +45,35 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/KodateMitsuru',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Kodate Mitsuru',
+  bio: 'SJTU CS undergraduates',
   links: [
+    // Visit https://icones.js.org/ for icon codes
+    // You will need to install the corresponding icon set if it's not already included
+    // `pnpm add @iconify-json/<icon-set-name>`
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/KodateMitsuru',
     },
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/profiles/76561199010381622',
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
-    },
+      name: 'Email',
+      icon: 'fa6-solid:envelope',
+      url: 'mailto:hh172834956@gamil.com',
+    }
   ],
 }
 
@@ -79,4 +81,21 @@ export const licenseConfig: LicenseConfig = {
   enable: true,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
+
+export const commentConfig: CommentConfig = {
+  giscus: {
+    repo: "KodateMitsuru/KodateMitsuru.github.io",
+    repoId: "R_kgDONfnILw",
+    category: "Comments",
+    categoryId: "DIC_kwDONfnIL84ClWsA",
+    mapping: 'title',
+    strict: '0',
+    reactionsEnabled: '1',
+    emitMetadata: '1',
+    inputPosition: 'top',
+    theme: 'light',
+    lang: 'zh-CN',
+    loading: 'lazy',
+  }
 }
---
+++ src/content/posts/HelloWorld.md
@@ -0,0 +1,24 @@
+---
+title: HelloWorld
+published: 2024-12-19
+description: ''
+image: ''
+tags: [HelloWorld]
+category: 'misc'
+draft: false 
+lang: ''
+---
+# HelloWorld
+
+使用astro开了个blog,以后估计往这里写周结
+
+# 原因
+
+* 看到有人往周结里塞网页，感觉挺好
+* 好久没搞了，捡起来
+* 被大佬安利了astro
+* ...
+
+# 其他
+
+原主题没有评论，从网上~抄了抄~搞了个Giscus,但是暗色支持没搞，看看后面有时间搞搞
